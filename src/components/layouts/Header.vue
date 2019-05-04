bd1b523676054cd32a22369339ea2f8e9dbc5064
jauhary <ms.jauhary@gmail.com>
2019-05-05 02:11:32 +0700

    header component

---
+++ src/components/layouts/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="niti-header">
+
+    </div>
+</template>
+<script>
+    
+    
+    export default {
+        mixins: [
+            
+        ],
+        components: {
+            
+        },
+        props: {
+            value: {}
+        },
+        data(){
+            return {
+                
+            }
+        },
+        created(){
+            
+        },
+        watch: {
+            
+        },
+        methods: {
+            
+        }
+    }
+</script>
+<style lang='scss'>
+    
+    
+</style>
